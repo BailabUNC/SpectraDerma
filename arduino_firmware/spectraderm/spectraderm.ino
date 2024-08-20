0f14b412676e8884326d40a56da9bec3c750d4ae
yihan-liu <y1hliu@unc.edu>
2024-08-20 13:35:40 -0400

    implement FreeRTOS

--- arduino_firmware/spectraderm/spectraderm.ino
+++ arduino_firmware/spectraderm/spectraderm.ino
@@ -1,8 +1,8 @@
 #include "Wire.h"
 #include "ArduinoBLE.h"
-#include "esp32-hal.h"
-
 #include <Adafruit_AS7341.h>
+
+#include "esp32-hal.h"
 
 #define CORE_DEBUG_LEVEL    3
 #define ESP_TAG             "SD"
@@ -20,7 +20,7 @@
 #define CHAR_SIZE           (CHANNEL_COUNT * BUFFER_SIZE * 6 + 1)
 
 hw_timer_t                  *timer = NULL;
-bool                        on_timer = false;
+SemaphoreHandle_t           sensorSemaphore;
 
 int channel_buffers[CHANNEL_COUNT][BUFFER_SIZE];
 int buffer_index = 0;
@@ -36,7 +36,52 @@ Adafruit_AS7341 as7341;
 
 void IRAM_ATTR onTimer()
 {
-    on_timer = true;
+    xSemaphoreGiveFromISR(sensorSemaphore, NULL);
+}
+
+void sensorTask(void *pvParameters)
+{
+    while (1)
+    {
+        if (xSemaphoreTake(sensorSemaphore, portMAX_DELAY) == pdTRUE)
+        {
+            digitalWrite(LED_CTRL, HIGH);
+
+            if (as7341.readAllChannels())
+            {
+                // Store sensor data in the buffer
+                channel_buffers[0][buffer_index] = as7341.getChannel(AS7341_CHANNEL_415nm_F1);
+                channel_buffers[1][buffer_index] = as7341.getChannel(AS7341_CHANNEL_445nm_F2);
+                channel_buffers[2][buffer_index] = as7341.getChannel(AS7341_CHANNEL_480nm_F3);
+                channel_buffers[3][buffer_index] = as7341.getChannel(AS7341_CHANNEL_515nm_F4);
+                channel_buffers[4][buffer_index] = as7341.getChannel(AS7341_CHANNEL_555nm_F5);
+                channel_buffers[5][buffer_index] = as7341.getChannel(AS7341_CHANNEL_590nm_F6);
+                channel_buffers[6][buffer_index] = as7341.getChannel(AS7341_CHANNEL_630nm_F7);
+                channel_buffers[7][buffer_index] = as7341.getChannel(AS7341_CHANNEL_680nm_F8);
+                channel_buffers[8][buffer_index] = as7341.getChannel(AS7341_CHANNEL_CLEAR);
+                channel_buffers[9][buffer_index] = as7341.getChannel(AS7341_CHANNEL_NIR);
+            }
+            buffer_index++;
+            digitalWrite(LED_CTRL, LOW);
+
+            if (buffer_index >= BUFFER_SIZE)
+            {
+                for (int i = 0; i < BUFFER_SIZE; i++)
+                {
+                    char data[CHAR_SIZE];
+                    char *pData = data;
+                    for (int j = 0; j < CHANNEL_COUNT; j++)
+                    {
+                        pData += sprintf(pData, "%d,", channel_buffers[j][i]);
+                    }
+                    *(pData - 1) = '\0'; // Remove the last comma and add a null terminator
+
+                    sdCharacteristic.writeValue(data);  // Send each line individually via BLE
+                }
+                buffer_index = 0;
+            }
+        }
+    }
 }
 
 void setup()
@@ -53,9 +98,12 @@ void setup()
 
     ESP_LOGI(ESP_TAG, "Info: Started.");
 
+    // Setup timer
     timer = timerBegin(1000000);
     timerAttachInterrupt(timer, &onTimer);
     timerAlarm(timer, ALARM_MS * 1000, true, 0);
+    
+    sensorSemaphore = xSemaphoreCreateBinary();
 
     // Setup AS7341
     if (!as7341.begin())
@@ -68,6 +116,9 @@ void setup()
     as7341.setATIME(29);
     as7341.setASTEP(59);
     as7341.setGain(AS7341_GAIN_128X);
+
+    // Setup sensor task
+    xTaskCreate(sensorTask, "Sensor task", 4096, NULL, 1, NULL);
 
     // Initialize BLE
     if (!BLE.begin())
@@ -95,44 +146,7 @@ void loop()
         ESP_LOGI(ESP_TAG, "Connected to central: %s", central.address().c_str());
         while (central.connected())
         {
-            if (on_timer)
-            {
-                digitalWrite(LED_CTRL, HIGH);
-
-                if (as7341.readAllChannels())
-                {
-                    // Map the channel index to the corresponding enum
-                    channel_buffers[0][buffer_index] = as7341.getChannel(AS7341_CHANNEL_415nm_F1);
-                    channel_buffers[1][buffer_index] = as7341.getChannel(AS7341_CHANNEL_445nm_F2);
-                    channel_buffers[2][buffer_index] = as7341.getChannel(AS7341_CHANNEL_480nm_F3);
-                    channel_buffers[3][buffer_index] = as7341.getChannel(AS7341_CHANNEL_515nm_F4);
-                    channel_buffers[4][buffer_index] = as7341.getChannel(AS7341_CHANNEL_555nm_F5);
-                    channel_buffers[5][buffer_index] = as7341.getChannel(AS7341_CHANNEL_590nm_F6);
-                    channel_buffers[6][buffer_index] = as7341.getChannel(AS7341_CHANNEL_630nm_F7);
-                    channel_buffers[7][buffer_index] = as7341.getChannel(AS7341_CHANNEL_680nm_F8);
-                    channel_buffers[8][buffer_index] = as7341.getChannel(AS7341_CHANNEL_CLEAR);
-                    channel_buffers[9][buffer_index] = as7341.getChannel(AS7341_CHANNEL_NIR);
-                }
-                buffer_index++;
-                digitalWrite(LED_CTRL, LOW);
-
-                if (buffer_index >= BUFFER_SIZE)
-                {
-                    for (int i = 0; i < BUFFER_SIZE; i++)
-                    {
-                        char data[CHAR_SIZE];
-                        char *pData = data;
-                        for (int j = 0; j < CHANNEL_COUNT; j++)
-                        {
-                            pData += sprintf(pData, "%d,", channel_buffers[j][i]);
-                        }
-                        *pData = '\0';
-                        sdCharacteristic.writeValue(data);
-                    }
-                    buffer_index = 0;
-                }
-                on_timer = false;
-            }
+            vTaskDelay(pdMS_TO_TICKS(10));  // Non-blocking delay
         }
         ESP_LOGI(ESP_TAG, "Disconnected from central: %s", central.address().c_str());
     }
