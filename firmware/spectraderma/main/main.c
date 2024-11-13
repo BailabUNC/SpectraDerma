d46bffca3e72cff3e2becaea09d2a77acb4e2930
yihan-liu <y1hliu@unc.edu>
2024-11-13 17:46:48 -0500

    update gatts

--- firmware/spectraderma/main/main.c
+++ firmware/spectraderma/main/main.c
@@ -317,7 +317,7 @@ static void gatts_event_handler(esp_gatts_cb_event_t event,
             break;
 
         case ESP_GATTS_WRITE_EVT:
-            ESP_LOGI(TAG, "GATT_WRITE_EVT, conn_id %d, trans_id %ld, handle %d\n", param->write.conn_id, param->write.trans_id, param->write.handle);
+            ESP_LOGI(TAG, "GATT_WRITE_EVT, conn_id %d, trans_id %" PRIu32 ", handle %d", param->write.conn_id, param->write.trans_id, param->write.handle);
             if (!param->write.is_prep)
             {
                 ESP_LOGI(TAG, "GATT_WRITE_EVT, value len %d, value: ", param->write.len);
